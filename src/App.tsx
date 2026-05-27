549d6b1d2204805be410b30055150d23c1c8fd13
yoon-12-12 <yjh20051212@gmail.com>
2026-05-27 20:42:07 +0900

    Fix Tailwind Vite plugin config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 
